8a525ab58cbff94a55ca6fa3073bc114e83f7fde
YarinNaftali <yarin9663@gmail.com>
2024-02-14 19:08:43 +0200

    Working simple file upload

--- backend/api/v1/files/__init__.py
+++ backend/api/v1/files/__init__.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, status
 
-router = APIRouter(
-    prefix="/files",
-)
+router = APIRouter()
 
-@router.post("/"
+
+@router.post("/upload-csv", status_code=status.HTTP_201_CREATED)
+async def upload_csv(file: UploadFile):
+    return {
+        "filename": file.filename,
+        "size": file.size,
+        "content_type": file.content_type,
+    }
