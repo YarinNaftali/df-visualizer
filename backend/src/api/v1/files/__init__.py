e209cecadcfad2a08684655d543e74763786116c
YarinNaftali <78485725+YarinNaftali@users.noreply.github.com>
2024-03-31 16:36:30 +0300

    Update __init__.py

--- backend/src/api/v1/files/__init__.py
+++ backend/src/api/v1/files/__init__.py
@@ -6,30 +6,33 @@ from uuid import uuid4
 router = APIRouter()
 
 
+@router.get("/all-files", status_code=status.HTTP_200_OK, response_model=list[str])
+async def get_all_files():
+    import os
+    files = os.listdir("uploads")
+    return files
+
+
 @router.post("/upload-csv", status_code=status.HTTP_201_CREATED)
 async def upload_csv(file: UploadFile):
-    if file.content_type != "text/csv":
+    try:
+        # Save the file to the uploads directory
+        # we can replace this routine with a call to S3 or any other storage service
+        file_name = f"{uuid4()}.csv"
+        with open(
+            f"uploads/{file_name}",
+            "wb",
+        ) as buffer:
+            buffer.write(await file.read())
+
         FilesLogDB.log_operation(
             Operation.UPLOAD,
-            status="FAILED",
-            message=f"File content type is not CSV. Got {file.content_type}",
+            status="SUCCESS",
+            message=f"File {file_name} uploaded successfully",
         )
-        return {"error": "Only CSV files are allowed"}
-    if not file.filename.endswith(".csv"):
-        FilesLogDB.log_operation(
-            Operation.UPLOAD,
-            status="FAILED",
-            message=f"File extension is not CSV. Got {file.filename}",
-        )
-        return {"error": "Only CSV files are allowed"}
 
-    # Save the file to the uploads directory
-    with open(
-        f"uploads/{uuid4()}.csv",
-        "wb",
-    ) as buffer:
-        buffer.write(await file.read())
+        return {"message": "File uploaded successfully"}
 
-    FilesLogDB.log_operation(Operation.UPLOAD, status="SUCCESS")
-
-    return {"message": "File uploaded successfully"}
+    except Exception as e:
+        FilesLogDB.log_operation(Operation.UPLOAD, status="FAILED", message=str(e))
+        return {"message": "File upload failed"}
